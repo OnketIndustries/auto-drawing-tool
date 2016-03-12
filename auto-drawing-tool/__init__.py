c5284d84e112f6379894bf26d3bd14e2a0b22192
wantanmen <wantan60@gmail.com>
2016-03-12 10:31:21 +0900

    Add wiki page link.

--- auto-drawing-tool/__init__.py
+++ auto-drawing-tool/__init__.py
@@ -23,13 +23,13 @@ import bpy
 bl_info = {
     "name": "Auto Drawing Tool",
     "author": "Nobuyuki Hirakata",
-    "version": (0, 1, 0),
+    "version": (0, 2, 0),
     "blender": (2, 76, 0),
     "location": "View3D > Toolshelf > Animation tab",
     "description": "Make auto-drawing setting using Build modifier and Freestyle.",
     "warning": "",
-    "wiki_url": "",
-    "tracker_url": "",
+    "wiki_url": "http://blenderartists.org/forum/showthread.php?394426-Addon-Auto-Drawing-Tool",
+    "tracker_url": "http://blenderartists.org/forum/showthread.php?394426-Addon-Auto-Drawing-Tool",
     "category": "Animation"
 }
 
